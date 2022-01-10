d9cd07a1a4ce0548566f8a51fd898a785f9ca178
jh79783 <jh79783@naver.com>
2022-01-10 16:45:16 +0900

    일반 함수 포인터 & 멤버 함수 포인터 다루기

---
+++ 27_tihscall4.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+
+// 스레드 클래스 설계하기
+
+// 스레드 리뷰
+// g++ 27_thiscall4.cpp -lpthread
+#if 0
+#include <pthread.h>
+
+void* foo(void* arg)
+{
+    cout << "foo" << endl;
+    return 0;
+}
+
+int main()
+{
+    pthread_t thread;
+    pthread_create(&thread, nullptr, &foo, nullptr);
+
+    pthread_join(thread, nullptr);  // 스레드 동작이 끝날때까지 대기
+
+    cout << "main end" << endl;
+}
+#endif
+
+// 스레드 클래스
+class Thread
+{
+    pthread_t thread;
+public:
+    virtual ~Thread() {}
+    // 모든 부모 클래스는 가상 소멸자를 제공해야한다.
+
+    void run()
+    {
+        // pthread_create(&thread, nullptr, &threadLoop, nullptr)  // pthread_create는 일반함수, threadLoop는 멤버함수 : 일반함수에 멤버함수를 담을 수 없다.    
+        pthread_create(&thread, nullptr, &_threadLoop, this);
+    }
+
+    void join()
+    {
+        pthread_join(thread, nullptr);
+    }
+
+    // 일반 함수 포인터에 멤버 함수 포인터는 담을 수 없지만, 정적 멤버 함수 포인터는 담을 수 있다.
+    static void* _threadLoop(void* arg)
+    {
+        Thread* self = static_cast<Thread*>(arg);
+
+        // 정적 멤버 함수 안에서 일반 멤버 함수를 호출하는 방법
+        self->threadLoop();
+
+        return 0;
+    }
+
+    // virtual bool threadLoop() { return false; }
+    virtual bool threadLoop() = 0;
+};
+
+
+
+// thread 라이브러리를 사용하는 방법.
+
+class MyThread : public Thread
+{
+public:
+    bool threadLoop() override
+    {
+        cout << "MyThread threadLoop" << endl;
+        return true;
+    }
+};
+
+int main()
+{
+    MyThread t;
+    t.run();    // 이 순간 스레드가 생성되어서, 가상함수인 threadLoop()를 수행해야한다.
+
+    t.join();
+}
